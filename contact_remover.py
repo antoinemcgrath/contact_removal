72d38c58028648551b40e6268dc94ac8cb43bcf9
Antoine McGrath <antoine.mcgrath@gmail.com>
2016-10-12 17:06:26 -0700

    Update contact_remover.py

--- contact_remover.py
+++ contact_remover.py
@@ -1,4 +1,6 @@
-#!/usr/bin/python
+# -*- encoding: utf-8 -*-
+
+#!/usr/bin/python3
 
 #### Single file run:
 # python contacts-remover.py input_file.pdf output_file.pdf
@@ -6,22 +8,49 @@
 #### Run on an entire directory:
 # python contacts-remover.py input_directory/ output_directory/
 
-#### This script will remove author contacts (telephone numbers and emails) from CRS PDFs
+#### This script will remove author contacts (telephone numbers, emails (and author names when provided in .txt)) from CRS PDFs
 # (Will not execute on hidden files nor subdirectories)
 # (Will halt script if a document is not a PDF or if PDF is corrupt)
 # (Destination directory will be created if it does not exist)
 # (Directories and document titles must not include spaces)
 
-##**QPDF is used to expand and condense CRS reports files it is released under 
-# **the terms of Version 2.0 of the Artistic License. Visit the site http://qpdf.sourceforge.net/ 
+##**QPDF is used to expand and condense CRS reports files it is released under
+# **the terms of Version 2.0 of the Artistic License. Visit the site http://qpdf.sourceforge.net/
 # **for additional information.
-
 
 import re
 import sys
 import os
 import subprocess
 import tempfile
+
+#Load author names (utf-8 characters only) 
+with open("author_names.txt", "r", encoding="utf-8") as f:
+    names = [line.rstrip('\n') for line in f]
+
+#author_file = open("author_names.txt", "r", encoding="utf-8")
+
+#global names
+#names = author_file.read().split('\\n')
+#name = names.encode('utf-8')#.decode().split('\\n')
+#name = names.decode()
+#nam = name.split('\\n')
+#author_file = author_file.encode('ascii', '.').decode('ascii')
+#names = author_file.read().split(',').encode('utf-8').strip()
+
+#print ((names.encode('utf-8')))
+
+#with open("author_names.txt", "r") as f:
+#author_file = author_file.decode("ascii")
+#    train_set, valid_set, test_set = f.load(f, encoding='latin1')
+
+#    names = f
+    #names = f.read().split(',')
+#    print (names)
+
+
+
+#read lines into an array
 
 #### Begin removing author contacts
 def remove_contacts_in_pdf(infile, outfile):
@@ -63,11 +92,11 @@ def remove_contacts_in_pdf(infile, outfile):
 
     #   Gets everything between "BT" and "ET" markers as text.
     #   WARNING WARNING: This may break if the PDF contains text that is "BT" or "ET"
-        for m in re.finditer(b'BT(.*?)ET', obj_data, re.DOTALL):
+        for m in re.finditer(rb'BT(.*?)ET', obj_data, re.DOTALL):
             the_text = m.group(1)
             text_start = m.start(1)
             # Find all the stuff inside the parentheses
-            for t in re.finditer(b'\(([^\(\)]+)\)', the_text):
+            for t in re.finditer(rb'\(([^\(\)]+)\)', the_text):
                 text_piece = t.group(1)
                 for idx in range(len(text_piece)):
                     text_map.append(obj_start + text_start + t.start(1) + idx)
@@ -78,9 +107,9 @@ def remove_contacts_in_pdf(infile, outfile):
     #   em Looks for email and the telephone within close proximity
     #   The following regex finds occurences of:
     #   @crs.loc.gov   @crs.loc.   @crs.lo.  @crsloc.gov   mailto:##@crs.loc.gov
-        for em in re.finditer(b'((\\b)|mailto:)\w+@crs(\.)?lo(\.)?(c\.(gov)?)?', master_text):
+        for em in re.finditer(rb'((\b)|mailto:)\w+@crs(\.)?lo(\.)?(c\.(gov)?)?', master_text):
             segment = master_text[em.start(0)-35:em.end(0)+35]
-            for emt in re.finditer(b'7-\d{4}', segment):
+            for emt in re.finditer(rb'7-\d{4}', segment):
                 what_was_removed.append(emt.group(0))
                 for idx in range(emt.start(0)+em.start(0)-35, emt.end(0)+em.start(0)-35):
                     data_idx = text_map[idx]
@@ -91,25 +120,64 @@ def remove_contacts_in_pdf(infile, outfile):
                 data_idx = text_map[idx]
                 data = data[:data_idx] + b" " + data[data_idx+1:]
 
+
+
+    #   Remove author names (as provided from file author_names.txt)
+
+        for named in names:
+            print (named)
+            print (named)
+            print (type(named))
+            #it = iter(named)
+            nameds = named.encode("utf-8")
+            #some_string.encode(encoding)
+
+            for authors_name in re.finditer(nameds, master_text):
+                segment = master_text[authors_name.start(0)-35:authors_name.end(0)+35]
+                print (segment)
+                print (segment)
+
+                for authors_namet in re.finditer(nameds, segment):
+                    what_was_removed.append(authors_namet.group(0))
+                    for idx in range(authors_namet.start(0)+authors_name.start(0)-35, authors_namet.end(0)+authors_name.start(0)-35):
+                        data_idx = text_map[idx]
+                        data = data[:data_idx] + b" " + data[data_idx+1:]
+        #    Removing em contact occurences
+                what_was_removed.append(authors_name.group(0))
+                for idx in range(authors_name.start(0), authors_name.end(0)):
+                    data_idx = text_map[idx]
+                    data = data[:data_idx] + b" " + data[data_idx+1:]
+
+
+
+
+
+
+
+
     #   cover Catches telephone contacts on the coverpage
-        for cover in re.finditer(b'(\\b)(www.crs.gov)', master_text):
+        for cover in re.finditer(rb'(\b)(www.crs.gov)', master_text):
             segment = master_text[cover.start(0)-35:cover.end(0)]
             #    Removing cover contact occurences
-            for cover_tele in re.finditer(b'7-\d{4}', segment):
+            for cover_tele in re.finditer(rb'7-\d{4}', segment):
                 what_was_removed.append(cover.group(0))
                 for idx in range(cover_tele.start(0)+cover.start(0)-35, cover_tele.end(0)+cover.start(0)-35):
                     data_idx = text_map[idx]
                     data = data[:data_idx] + b" " + data[data_idx+1:]
 
     #   ack catches telephone contacts within Acknowledgment sections
-        for ack in re.finditer(b'(\\b)(Acknowledgments)', master_text):
+        for ack in re.finditer(rb'(\b)(Acknowledgments)', master_text):
             segment = master_text[ack.start(0):ack.end(0)+500]
-            for ack_tele in re.finditer(b'7-\d{4}', segment):
+            for ack_tele in re.finditer(rb'7-\d{4}', segment):
                 #    Removing ack contact occruences
                 what_was_removed.append(ack.group(0))
                 for idx in range(ack.start(0)+ack_tele.start(0), ack_tele.end(0)+ack.start(0)):
                     data_idx = text_map[idx]
                     data = data[:data_idx] + b" " + data[data_idx+1:]
+
+
+
+
 
     #### Compress the new author-contact-free CRS file and write to the output file.
     with tempfile.NamedTemporaryFile() as f:
